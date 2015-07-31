9266e43ef2b667c722369ba67e931e59b84caeec
t3knoid <t3knoid@yahoo.com>
2015-07-31 06:27:24 -0400

    Launch application after installation
    Added installer class with appropriate Commited install event handler.
    The event handler launches the application at the end of the install.

---
+++ KeepAwake/Installer.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.Collections;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Reflection;
+using System.IO;
+
+namespace KeepAwake
+{
+    // Taken from:http://msdn2.microsoft.com/en-us/library/
+    // system.configuration.configurationmanager.aspx
+    // Set 'RunInstaller' attribute to true.
+
+    [RunInstaller(true)]
+    public partial class Installer : System.Configuration.Install.Installer
+    {
+        public Installer()
+            : base()
+        {
+            // Attach the 'Committed' event.
+            this.Committed += new InstallEventHandler(MyInstaller_Committed);
+            // Attach the 'Committing' event.
+            this.Committing += new InstallEventHandler(MyInstaller_Committing);
+        }
+
+        // Event handler for 'Committing' event.
+        private void MyInstaller_Committing(object sender, InstallEventArgs e)
+        {
+
+            //Console.WriteLine("");
+            //Console.WriteLine("Committing Event occurred.");
+            //Console.WriteLine("");
+        }
+
+        /// <summary>
+        /// Event handler for 'Committed' event. This event will launch the application
+        /// After the installer has completed.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MyInstaller_Committed(object sender, InstallEventArgs e)
+        {
+            try
+            {
+                Directory.SetCurrentDirectory(Path.GetDirectoryName
+                (Assembly.GetExecutingAssembly().Location));
+                Process.Start(Path.GetDirectoryName(
+                    Assembly.GetExecutingAssembly().Location) + "\\KeepAwake.exe");
+            }
+            catch
+            {
+                // Do nothing... 
+            }
+        }
+
+        // Override the 'Install' method.
+        public override void Install(IDictionary savedState)
+        {
+            base.Install(savedState);
+        }
+
+        // Override the 'Commit' method.
+        public override void Commit(IDictionary savedState)
+        {
+            base.Commit(savedState);
+        }
+
+        // Override the 'Rollback' method.
+        public override void Rollback(IDictionary savedState)
+        {
+            base.Rollback(savedState);
+        }
+    }
+}
